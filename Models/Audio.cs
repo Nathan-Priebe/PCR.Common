be62062a59e46ac1b0da728bac53d1111ff41daa
Nathan Priebe <31726704+Nathan-Priebe@users.noreply.github.com>
2017-09-07 19:33:39 +1000

    Intial Commit

---
+++ Models/Audio.cs
@@ -0,0 +1,15 @@
+﻿namespace PCR.Common.Models
+{
+    public class AppDetails
+    {
+        public string App { get; set; }
+        public uint ProcessId { get; set; }
+        public int Volume { get; set; }
+        public bool Mute { get; set; }
+
+        public AppDetails()
+        {
+            Mute = false;
+        }
+    }
+}
