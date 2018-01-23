4d1fac30e40ad7fec58510d7301cdd76e2d400a9
Nathan Priebe <nathan_priebe@hotmail.com>
2018-01-24 08:45:33 +1000

    Added code that could be shared from Android project here so that it could be shared with iOS project. Code included web request and audio modification code.

---
+++ Audio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PCR.Common
+{
+    public class Audio
+    {
+        public static async Task<List<T>> GetAudioStatus<T>(string url)
+        {
+            var appList = new List<T>();
+
+            try
+            {
+                var response = await WebRequest.GetWebRequest(url, "Audio/All");
+                var responseBody = await response.Content.ReadAsStringAsync();
+                appList = JsonConvert.DeserializeObject<List<T>>(responseBody);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Unable to retrieve audio session, check server is running and try again");
+            }
+            return appList;
+        }
+
+        public static async Task UpdateAppVolume<T>(string url, T message)
+        {
+            try
+            {
+                await WebRequest.PostWebRequest(url, "Audio/Update", message);
+            }
+            catch (Exception e)
+            {
+                await WebRequest.PostWebRequest(url, "System/UpdateLog", e.Message);
+            }
+        }
+
+        public static async Task MuteApp(string url, uint processId)
+        {
+            try
+            {
+                await WebRequest.PostWebRequest(url, "Audio/Mute", processId);
+            }
+            catch (Exception e)
+            {
+                await WebRequest.PostWebRequest(url, "System/UpdateLog", e.Message);
+            }
+        }
+    }
+}
---
+++ WebRequest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ModernHttpClient;
+using Newtonsoft.Json;
+
+namespace PCR.Common
+{
+    public class WebRequest
+    {
+        public static async Task<HttpResponseMessage> GetWebRequest(string ipAddress, string endPoint)
+        {
+            CancellationTokenSource cts;
+            Task<HttpResponseMessage> reqTask;
+            using (var httpClient = new HttpClient(new NativeMessageHandler()))
+            {
+                httpClient.Timeout = TimeSpan.FromSeconds(30);
+                cts = new CancellationTokenSource();
+                cts.CancelAfter(TimeSpan.FromSeconds(30));
+                reqTask = httpClient.GetAsync(ipAddress + endPoint, cts.Token);
+            }
+
+            HttpResponseMessage response;
+            try
+            {
+                if (Task.WaitAny(new Task[] { reqTask }, 30000) < 0)
+                {
+                    cts.Cancel(); // attempt to cancel the HTTP request
+                    throw new Exception("The network request timed out. Please check your network connection.");
+                }
+                response = reqTask.GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+
+        public static async Task<HttpResponseMessage> PostWebRequest<T>(string ipAddress, string endPoint, T body)
+        {
+            var output = JsonConvert.SerializeObject(body);
+            var httpContent = new StringContent(output, Encoding.UTF8, "application/json");
+
+            var httpClient = new HttpClient(new NativeMessageHandler());
+            httpClient.Timeout = TimeSpan.FromSeconds(30);
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(30));
+            var reqTask = httpClient.PostAsync(ipAddress + endPoint, httpContent, cts.Token);
+            HttpResponseMessage response;
+
+            try
+            {
+                if (Task.WaitAny(new Task[] { reqTask }, 30000) < 0)
+                {
+                    cts.Cancel(); // attempt to cancel the HTTP request
+                    throw new Exception("The network request timed out. Please check your network connection.");
+                }
+                response = reqTask.GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+    }
+}
